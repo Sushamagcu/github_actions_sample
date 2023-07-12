c51f1d5f804732e7da3e7b4cff843b1b59f0e390
sushamagcu@gmail.com <sushamagcu@gmail.com>
2023-07-12 16:21:35 +0100

    maven yml changes

--- src/main/java/com/sample/SpingbootGithubActionsApplication.java
+++ src/main/java/com/sample/SpingbootGithubActionsApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class SpingbootGithubActionsApplication {
-
+//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
 	@GetMapping("/welcome")
 	public String welcome() {
 		return "Hello welcome to Spring boot application";
